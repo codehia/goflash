193a888ac90ec3515bc28e9b5a2dae5563c2a59e
codehia <dev@sacharya.dev>
2026-06-25 00:07:35 +0530

    add some util GetEnv util function

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"fmt"
+	"os"
+)
+
 func StringPtrEqual(a, b *string) bool {
 	if a == nil && b == nil {
 		return true
@@ -8,4 +13,14 @@ func StringPtrEqual(a, b *string) bool {
 		return false
 	}
 	return *a == *b
+}
+
+func GetEnv(key string, fallback ...string) (string, error) {
+	if value, ok := os.LookupEnv(key); ok {
+		return value, nil
+	}
+	if len(fallback) > 0 {
+		return fallback[0], nil
+	}
+	return "", fmt.Errorf("env var %q is not set", key)
 }
